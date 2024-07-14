392ec881ebb6902309bfb9464a4759c71be04552
Codfisher <pony2140@livemail.tw>
2024-07-14 14:25:17 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DialogTemp from './components/DialogTemp.vue';
+
+import { openUsingDialog } from './common/utils-quasar';
+
+function openEditForm() {
+  openUsingDialog(DialogTemp);
+}
+</script>
+
+<template>
+  <div>安安</div>
+
+  <q-btn @click="openEditForm()">按鈕</q-btn>
+</template>
+
+<style scoped></style>
